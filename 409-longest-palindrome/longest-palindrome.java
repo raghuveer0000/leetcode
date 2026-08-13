2ad898b2d5b7ac47bdc10e4078fad49be7d23433
Narsapur Raghuveer <raghuveernarsapur@gmail.com>
2026-08-13 22:54:13 +0530

    Time: 8 ms (28.93%) | Memory: 43.4 MB (28.77%) - LeetSync

---
+++ 409-longest-palindrome/longest-palindrome.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int longestPalindrome(String s) {
+        Map<Character, Integer> freq = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            freq.put(c, freq.getOrDefault(c, 0) + 1);
+        }
+        int length = 0;
+        boolean hasOdd = false;
+        for (int count : freq.values()) {
+            if (count % 2 == 0) {
+                length += count;
+            } else {
+                length += count - 1;
+                hasOdd = true;
+            }
+        }
+        return hasOdd ? length + 1 : length;
+    }
+}
